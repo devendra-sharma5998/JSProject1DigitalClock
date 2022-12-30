fb8fef1a846dd0c64d6b939a53235e7002558852
devendra-sharma5998 <devendrasharma5998@gmail.com>
2022-12-30 17:44:15 +0530

    modified

--- index.js
+++ index.js
@@ -1,101 +1,121 @@
-let clock= document.getElementById("clock2")
-let hr= document.getElementById("hr")
-let min= document.getElementById("min")
-let sec= document.getElementById("sec")
-let am_pm= document.getElementById("am_pm")
+let clock=document.getElementById("clock2")
 
-function timer(){
-    let timers=new Date();
-    let hours=timers.getHours()
-    let minutes=timers.getMinutes()
-    let seconds=timers.getSeconds()
+let hr=document.getElementById("hr")
+let min=document.getElementById("min")
+let sec=document.getElementById("sec")
+let am_pm=document.getElementById("am_pm")
+let image_text=document.getElementById("image_text")
+
+
+function timers(){
     
-   
-    min.innerText=minutes;
+    let timer=new Date();
+    let hour=timer.getHours();
+    let minute=timer.getMinutes();
+    let seconds=timer.getSeconds();
+    
+    min.innerText=minute;
     sec.innerText=seconds;
-    if(hours>=12 ){
+
+    if(hour>=12){
         am_pm.innerText="PM"
     }
-    let t =document.getElementById("food_timing")
-    let j=document.getElementById("goodtext")
-    let image=document.getElementById("image")
-    
-    if(hours>12 && hours<14){
-        food_timing.innerText="grab some health Lunch"
-        goodtext.innerText="GOOD AFTERNOON!!TAKE SOME SLEEP"
-        image.style.backgroundimage="URL('./Component 31 – 1@2x.jpg')"
+    if(hour>=12 && hour<16){
+        image_text.innerText="LETS HAVE LUNCH"
     }
-    if(hours>14 && hours<18){
-        food_timing.innerText="STOP YAWINING, GET SOME TEA. "
-        goodtext.innerText="GOOD EVENING"
-        image.style.backgroundImage="URL('./lunch_image@2x.png')"
+    if(hour>=16 && hour<20){
+        image_text.innerText="STOP YAWNING,GET SOME TEA.. ITS JUST EVENING!"
     }
-    if(hours>18 && hours<22 ){
-        food_timing.innerText="CLOSE YOUR EYES AND GO TO SLEEP"
-        goodtext.innerText="GOOD NIGHT"
-        // image.style.backgroundimage="URL('./goodnight_image@2x.jpg')"
-        image.style.backgroundImage="URL('./goodnight_image@2x.jpg')"
+    if(hour>=20 && hour<24){
+        image_text.innerText="CLOSE YOUR EYES AND GO TO SLEEP"
     }
+    // }
+    if(hour>12){
+     hr.innerText=hour-12;
 
-    // console.log(timers);
-    if(hours>12){
-        hours= hours-12
-    }
-    hr.innerText=hours;
+ }
+    // console.log(timer);
+
 }
+setInterval(timers,1000);
 
-setInterval(timer,1000);
+let wakeup=document.getElementById("selectWakeUp")
+let wakeUpvalue;
+let lunch=document.getElementById("selectlunch")
+let lunchvalue;
+let nap=document.getElementById("selectnap")
+let napvalue;
+let night=document.getElementById("selectnight")
+let nightvalue;
 
-// let btns=docuent.getElementById("butn")
+let morning_text=document.getElementById("morning_text")
+let lunch_text=document.getElementById("lunch_text");
+let nap_text=document.getElementById("nap_text");
+let night_text=document.getElementById("night_text");
+ 
+let morn;
+let lunn;
+let napp;
+let nightt;
 
-
-// selectbutn.addEventListener("change",function(event){
-    let morning=document.getElementById("morning")
-    let lunch = document.getElementById("lunch")
-    let nap = document.getElementById("nap")
-    let night = document.getElementById("night")
-// })
-let butn =document.getElementById("butn")
-let morningtext;
-let lunchtext;
-let naptext;
-let nighttext;
-
-    // button= document.getElementById("button",function(event){
-
-select.addEventListener("change",function(event){
-    // console.log(event.target.selectedOptions[0].text) 
-   morningtext=`WakeupTime ${event.target.selectedOptions[0].text}`
-    
+wakeup.addEventListener("change",function(event){
+    morn=`wakeup_time:${event.target.selectedOptions[0].text}`
+    wakeUpvalue=event.target.selectedOptions[0].value;
+//    console.log(morn,wakeUpvalue)
 })
-selectlunch.addEventListener("change",function(event){
-    // console.log(event.target.selectedOptions[0].text) 
-    lunchtext=`LunchTime ${event.target.selectedOptions[0].text}`
-    
+lunch.addEventListener("change",function(event){
+    lunn=`lunch_time:${event.target.selectedOptions[0].text}`
+    lunchvalue=event.target.selectedOptions[0].value;
 })
-selectnap.addEventListener("change",function(event){
-    // console.log(event.target.selectedOptions[0].text) 
-    naptext=`NapTime ${event.target.selectedOptions[0].text}`
-    
+nap.addEventListener("change",function(event){
+    napp=`nap_time ${event.target.selectedOptions[0].text}`
+    napvalue=event.target.selectedOptions[0].value;
 })
-selectnight.addEventListener("change",function(event){
-    // console.log(event.target.selectedOptions[0].text) 
-    nighttext=`NightTime ${event.target.selectedOptions[0].text}`
-    
+night.addEventListener("change",function(event){
+    nightt=`night_time ${event.target.selectedOptions[0].text}`
+    nightvalue=event.target.selectedOptions[0].value;
 })
 
+let ghanta=new Date()
+let gha_hr=ghanta.getHours();
+
+ghanta=gha_hr
+if(gha_hr>12){
+    gha_hr=gha_hr-12
+}
+console.log(gha_hr)
+let goodtext=document.getElementById("goodtext")
+let image=document.getElementById("image")
+
+let butn=document.getElementById("butn")
 butn.addEventListener("click",function(){
-   morning.innerText=morningtext 
+morning_text.innerText=morn;
+lunch_text.innerText=lunn;
+nap_text.innerText=napp;
+night_text.innerText=nightt;
+
+console.log(gha_hr,wakeUpvalue,ghanta)
+
+if(ghanta==wakeUpvalue){
+goodtext.innerText="GOOD MORNING!! WAKE UP!!";
+image.style.backgroundImage="URL('./Component 30 – 1')"
+}
+else if(ghanta==lunchvalue){
+    goodtext.innerText="GOOD AFTERNOON !! HAVE SOME SLEEP";
+    image.style.backgroundImage="URL('./Component 31 – 1@2x.jpg')"
+}
+else if(ghanta==napvalue){
+    goodtext.innerText="GOOD EVENING!!"
+    image.style.backgroundImage="URL('./lunch_image@2x.png')"
+}
+else if(ghanta==nightvalue){
+    goodtext.innerText="GOOD NIGHT!!"
+    image.style.backgroundImage="URL('./goodnight_image@2x.jpg')"
+}
 })
-butn.addEventListener("click",function(){
-lunch.innerText=lunchtext;
-})
-butn.addEventListener("click",function(){
-nap.innerText=naptext;
-})
-butn.addEventListener("click", function(){
-    night.innerText=nighttext
-})
+
+
+
 
 
 
